1818ba5d3a472c04a8ae6e8d2a66802adcc33375
Marcus Brito <marcus@bri.to>
2023-03-08 14:33:10 -0300

    Use `ServeDir` to serve static files

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ use std::{convert::Infallible, error::Error, sync::Arc};
 use axum::{
     debug_handler,
     extract::State,
-    http::header,
     response::{
         sse::{Event, KeepAlive},
-        Html, IntoResponse, Response, Sse,
+        Sse,
     },
     routing::get,
     Router, Server,
@@ -15,6 +14,7 @@ use futures::Stream;
 use sysinfo::{CpuExt, System, SystemExt};
 use tokio::sync::watch::{self, Sender};
 use tokio_stream::{wrappers::WatchStream, StreamExt};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> Result<(), impl Error> {
@@ -22,10 +22,8 @@ async fn main() -> Result<(), impl Error> {
     let state = AppState { tx: tx.clone() };
 
     let router = Router::new()
-        .route("/", get(root_get))
-        .route("/index.mjs", get(index_mjs_get))
-        .route("/index.css", get(index_css_get))
         .route("/api/cpus", get(cpus_get))
+        .fallback_service(ServeDir::new("assets"))
         .with_state(state.clone());
 
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
@@ -53,30 +51,6 @@ type Snapshot = Vec<f32>;
 struct AppState {
     // Sender is thread-safe, no need to wrap in Mutex/RwLock
     tx: Arc<Sender<Snapshot>>,
-}
-
-#[debug_handler]
-async fn root_get() -> impl IntoResponse {
-    let str = tokio::fs::read_to_string("src/index.html").await.unwrap();
-    Html(str)
-}
-
-#[debug_handler]
-async fn index_mjs_get() -> impl IntoResponse {
-    let str = tokio::fs::read_to_string("src/index.mjs").await.unwrap();
-    Response::builder()
-        .header(header::CONTENT_TYPE, "application/javascript;charset=utf-8")
-        .body(str)
-        .unwrap()
-}
-
-#[debug_handler]
-async fn index_css_get() -> impl IntoResponse {
-    let str = tokio::fs::read_to_string("src/index.css").await.unwrap();
-    Response::builder()
-        .header(header::CONTENT_TYPE, "text/css;charset=utf-8")
-        .body(str)
-        .unwrap()
 }
 
 #[debug_handler]
